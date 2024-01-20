b8c705a238e84dc75c8c4816ad07413d8b828091
Mohakad <mohkadtrd@gmail.com>
2024-01-20 16:39:42 +0300

    bub

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,9 +10,12 @@ void bubble_sort(int *array, size_t size)
 
 	int tmp;
 
+	if (size < 2 || array == NULL)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < i - 1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
