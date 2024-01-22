f33adee3b1f24a92a2de647acee02f8e01fe44f4
Mohakad <mohkadtrd@gmail.com>
2024-01-22 20:36:33 +0300

    1-o

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,26 +7,26 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *temp;
 
-	if (list == NULL || *list == NULL)
+	if ((*list)->next == NULL || list == NULL || *list == NULL)
 		return;
 	current = (*list)->next;
 	while (current != NULL)
 	{
 		temp = current;
-		 current = current->next;
-		 while (temp->prev != NULL && temp->prev->n > temp->n)
-		 {
-			 temp->prev->next = temp->next;
-			 if (temp->next != NULL)
-				 temp->next->prev = temp->prev;
-			 temp->next = temp->prev;
-			 temp->prev = temp->prev->prev;
-			 temp->next->prev = temp;
-			 if (temp->prev != NULL)
-				 temp->prev->next = temp;
-			 else
-				 *list = temp;
-			 print_list((const listint_t *)*list);
-		 }
+		current = current->next;
+		while (temp->prev != NULL && temp->prev->n > temp->n)
+		{
+			temp->prev->next = temp->next;
+			if (temp->next != NULL)
+				temp->next->prev = temp->prev;
+			temp->next = temp->prev;
+			temp->prev = temp->prev->prev;
+			temp->next->prev = temp;
+			if (temp->prev != NULL)
+				temp->prev->next = temp;
+			else
+				*list = temp;
+			print_list((const listint_t *)*list);
+		}
 	}
 }
